e3b0ad8c782e05c9fa2a92f753d62fdf6aff1cb5
Unknown <stmihailov@yandex.ru>
2018-10-22 05:00:46 +0300

    тест

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 jQuery(document).ready(function($){
-	$("#navbar-nav").on("click", "a", function (event) {
+	$('#navbar-nav').on('click', 'a', function (event) {
 		var id  = $(this).attr('href'),
 			top = $(id).offset().top - 100;
 		$('body,html').animate({scrollTop: top}, 1000);
 		console.log(this)
  if(this.hash == id){
 	 $('.nav-link').removeClass('active');
-	 $(this).addClass("active");
+	 $(this).addClass('active');
  }
 	});
 });
@@ -26,7 +26,7 @@ var swiper = new Swiper('.swiper-container', {
 });
 let parralax = document.querySelector('.requirement');
 window.onscroll  = function () {
-if (window.pageYOffset > 700 && window.pageYOffset < 1100){
+if (window.pageYOffset > 700 && window.pageYOffset < 1200){
 let yOffset = window.pageYOffset - 700;
 parralax.style.backgroundPosition = '0px '+ -yOffset + 'px';
 }
